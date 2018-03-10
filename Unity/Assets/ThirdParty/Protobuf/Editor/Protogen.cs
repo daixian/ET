f5b64738a6b622f827465fd65a005ed56e4fff28
daixian <amano_tooko@qq.com>
2018-03-10 14:08:03 +0800

    调试了一下proto文件生成cs

--- Unity/Assets/ThirdParty/Protobuf/Editor/Protogen.cs
+++ Unity/Assets/ThirdParty/Protobuf/Editor/Protogen.cs
@@ -12,7 +12,7 @@ public class Protogen {
     public static void GenerateProtobufCS()
     {
         //这句看起来是要生成mmopb.proto文件，但是这个文件在路径中并不存在
-        Generate(Application.dataPath + "/../Proto/",new string[] { "mmopb.proto" }, Application.dataPath + "/../HotFix/");
+        Generate(Application.dataPath + "/../../Proto/", new string[] { "mmopb.proto" }, Application.dataPath + "/../HotFix/");
     }
     static void Generate(string inpath,string[] inprotos,string outpath)
     {
